676a6a22b35de88db09e1b4e440412ed8757cede
caucas <georgy.davityan@gmail.com>
2016-05-14 17:50:34 +0300

    Use UserDetailsService interface as autowired field instead of UserDetailsServiceImpl in WebSecurityConfiguration

--- src/main/java/cc/caucas/lunch/common/config/WebSecurityConfiguration.java
+++ src/main/java/cc/caucas/lunch/common/config/WebSecurityConfiguration.java
@@ -1,12 +1,12 @@
 package cc.caucas.lunch.common.config;
 
-import cc.caucas.lunch.common.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @author Georgy Davityan.
@@ -16,7 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
